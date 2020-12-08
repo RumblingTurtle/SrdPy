081dedd616530debc0f3e2d91c0f66ac269bad81
rumblingturtle <edzaljaev@gmail.com>
2020-12-08 16:56:20 +0300

    Added linearization handlers

--- SymbolicUtils/getGeneralizedCoordinatesModelHandlers.py
+++ SymbolicUtils/getGeneralizedCoordinatesModelHandlers.py
@@ -3,9 +3,14 @@ from collections import namedtuple
 
 def getGeneralizedCoordinatesModelHandlers(description):
     so_path = description["path"]+"/"+description["casadi_cCodeFilename"]+".so"
-    handler = namedtuple("GeneralizedCoordinatesModelHandler","getJointSpaceInertiaMatrix getBiasVector getControlMap")
+    fields = ["getJointSpaceInertiaMatrix","getBiasVector","getControlMap","dofConfigurationSpaceRobot","dofControl"]
+
+    handler = namedtuple("GeneralizedCoordinatesModelHandler",fields)
+
+    dofConfigurationSpaceRobot = description["dofConfigurationSpaceRobot"]
+    dofControl = description["dofControl"]
     getJointSpaceInertiaMatrix = external(description["functionName_H"],so_path)
     getBiasVector = external(description["functionName_c"],so_path)
     getControlMap = external(description["functionName_T"],so_path)
 
-    return handler(getJointSpaceInertiaMatrix,getBiasVector,getControlMap)
+    return handler(getJointSpaceInertiaMatrix,getBiasVector,getControlMap,dofConfigurationSpaceRobot,dofControl)
---
+++ SymbolicUtils/getLinearizedModelHandlers.py
@@ -0,0 +1,17 @@
+from casadi import *
+from collections import namedtuple
+
+def getLinearizedModelHandlers(description):
+    so_path = description["path"] + "/" + description["casadi_cCodeFilename"] + ".so"
+    fields = ["getA", "getB", "getC", "dofConfigurationSpaceRobot","dofStateSpaceRobot","dofControl"]
+
+    handler = namedtuple("LinearizedModelHandlers", fields)
+
+    dofConfigurationSpaceRobot = description["dofConfigurationSpaceRobot"]
+    dofStateSpaceRobot = description["dofStateSpaceRobot"]
+    dofControl = description["dofControl"]
+    getA = external(description["functionName_A"], so_path)
+    getB = external(description["functionName_B"], so_path)
+    getC = external(description["functionName_c"], so_path)
+
+    return handler(getA, getB, getC, dofConfigurationSpaceRobot, dofStateSpaceRobot, dofControl)
