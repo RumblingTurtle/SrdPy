e756d56dc01a71d4c7b74db4f9cb6e2ff9c81236
rumblingturtle <edzaljaev@gmail.com>
2020-12-17 23:44:02 +0300

    Finalized state logging

--- Loggers/getStateLoggerHandler.py
+++ Loggers/getStateLoggerHandler.py
@@ -1,4 +1,5 @@
 import numpy as np
+from casadi import *
 class StateLoggerHandler():
     def __init__(self,stateHandler,simulationHandler,logAcceleration):
         self.stateHandler = stateHandler
@@ -11,11 +12,11 @@ class StateLoggerHandler():
     def update(self):
         i = self.simulationHandler.currentIndex
 
-        self.q.append(self.stateHandler.q)
-        self.v.append(self.stateHandler.v)
+        self.q.append(np.array(DM(self.stateHandler.q)).T[0])
+        self.v.append(np.array(DM(self.stateHandler.v)).T[0])
 
         if self.logAcceleration:
-            self.a.append(self.stateHandler.a)
+            self.a.append(np.array(DM(self.stateHandler.a)).T[0])
 
 
 
